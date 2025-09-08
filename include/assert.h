deadf441caf62682c4b6ba83ac886ac95d1cbd8d
Tobias Hennerbichler <hennerbichler.tobias@gmx.at>
2025-09-08 19:05:15 +0200

    git rm files

--- include/assert.h
+++
@@ -1,9 +0,0 @@
-#ifndef __ASSERT_H__
-#define __ASSERT_H__
-
-#include <stdbool.h>
-
-void assert(bool cond);
-void assert_msg(bool cond, const char *err_msg);
-
-#endif
--- include/stdlib.h
+++
@@ -1,9 +0,0 @@
-#include __STDLIB_H__
-#define __STDLIB_H__
-
-void *malloc(size_t size);
-void free(void *ptr);
-void *calloc(size_t nmemb, size_t size);
-void *realloc(void *ptr, size_t size);
-
-#endif
